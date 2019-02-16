84fe92c6607178e65b58ac0826e375d7af048381
root <root@DESKTOP-S3NV0P1.localdomain>
2019-02-15 23:10:43 -0500

    Added temporary Adobe service kills

--- FreshenUp.bat
+++ FreshenUp.bat
@@ -1,3 +1,5 @@
+sc stop AGMService
+sc stop AGSService
 sc stop TabletInputService
 sc stop AdobeARMservice
 sc stop BTAGService
@@ -5,8 +7,11 @@ sc stop bthserv
 sc stop wuauserv
 taskkill -t -f -im Adobe*
 taskkill -t -f -im Creative*
+taskkill -t -f -im Acro*
 taskkill -t -f -im acro*
 taskkill -t -f -im Skype*
 taskkill -t -f -im ctfmon*
 taskkill -t -f -im Microsoft.Photos*
+taskkill -t -f -im OneDrive*
+taskkill -t -f -im WinStore*
 pause
