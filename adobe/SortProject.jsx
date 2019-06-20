15e2e8d0310d0db25b52bbffd723a54f899d36e9
Patrick Brady <patrick.brady@gmail.com>
2019-06-20 09:08:38 -0400

    refactor SortProject to ES3

--- adobe/SortProject.jsx
+++ adobe/SortProject.jsx
@@ -13,7 +13,7 @@
 
 (function()
 {
-    const FOLDER_NAMES = {
+    var FOLDER_NAMES = {
         COMPS: 'Comps',
         PRECOMPS: '_Precomps',
         SOLIDS: 'Solids',
@@ -22,7 +22,7 @@
 
     function getAllItems()
     {
-        const a = new Array();
+        var a = new Array();
         var proj = app.project;
         for (var i = 1; i <= proj.numItems; i++) {
             a.push(proj.item(i));
@@ -31,44 +31,48 @@
     };
 
     function getFolders(){
-        const items = app.project.items;
-        const folders = {};
-        Object.keys(FOLDER_NAMES).map((key) => FOLDER_NAMES[key]).forEach((name) => {
+        var items = app.project.items;
+        var folders = {};
+        for(var k in FOLDER_NAMES) {
+            var name = FOLDER_NAMES[k];
             folders[name] = (items.addFolder(name));
-        });
+        }
         return folders;
     }
 
     function organize(){
-        const folders = getFolders();
-        const items = getAllItems();
-        items.forEach((item) => {
-            if (!FOLDER_NAMES.includes(item.name)) {
-                let folderName = FOLDER_NAMES.COMPS
+        var folders = getFolders();
+        var items = getAllItems();
+        var sortItem = function (item) {
+            if (item.typeName !== 'Folder') {
+                var folderName = FOLDER_NAMES.COMPS
                 if (item.typeName === 'Footage') {
                     folderName = item.mainSource instanceof SolidSource ? FOLDER_NAMES.SOLIDS : FOLDER_NAMES.ASSETS;
                 }
                 else if (item.typeName === 'Composition' && item.usedIn.length > 0) {
                     folderName = FOLDER_NAMES.PRECOMPS;
                 }
-                item.parentFolder = folders[folderName]
+                item.parentFolder = folders[folderName];
             }
-        });
+        };
+        for (var i = 0; i < items.length; i++) {
+            sortItem(items[i]);
+        }
     }
 
     function cleanUp(){
-        getAllItems()
-        .filter((item) => item.typeName === "Folder")
-        .forEach((folder) => {
-            if (folder.numItems === 0) folder.remove();
-        })
+        var items = getAllItems();
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            if (item.typeName === "Folder" && item.numItems === 0) item.remove();
+        }
     }
 
     function main()
     {
         app.beginUndoGroup("Sort Project");
-        organize()
-        cleanUp()
+        organize();
+        cleanUp();
 
         app.endUndoGroup();
     }
